a9eeb008a25d1a53f0d019a5e4e3208ba1c0797b
Nathan-dOS <nathanods@gmail.com>
2026-01-28 11:36:20 -0300

    chore: stop tracking deploy_pkg folder

--- deploy_pkg/ai_engine/tasks.py
+++
@@ -1,2 +0,0 @@
-# Tasks are currently defined dynamically in main.py
-# This file is reserved for future reusable task definitions.
--- deploy_pkg/frontend/src/components/landing/features/ScalableSupportAnimation.tsx
+++
@@ -1,6 +0,0 @@
-import Lottie from "lottie-react";
-import scalableSupportAnimation from "../../../assets/animations/scalable-support.json";
-
-export const ScalableSupportAnimation = () => {
-    return <Lottie animationData={scalableSupportAnimation} loop={true} className="w-full h-full" />;
-};
